921575789e798a362f020cb6c65a8b19ddcf38b3
Sergio San Pedro <sergiosanpedro@sergios-imac.local>
2019-01-31 04:32:50 -1000

    Segue Press button to Second View Controller achieved. Change Color button to blue achieved

---
+++ HackwichTwo/SecondViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SecondViewController.swift
+//  HackwichTwo
+//
+//  Created by Sergio San Pedro on 1/31/19.
+//  Copyright © 2019 Sergio San Pedro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+        
+        //on button press, we want to the background view to turn blue
+        
+        self.view.backgroundColor=UIColor.blue
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
